54f44e6634f0ce6d6888640bdd15addeecaa1ef1
medilies <medilies@gmail.com>
2023-12-28 20:31:31 +0100

    refactor and style compileControlComponents

--- src/CtrlP.php
+++ src/CtrlP.php
@@ -169,44 +169,6 @@ class CtrlP
     // ControlComponents
     // ========================================================================
 
-    protected function printButtonComponent(): string
-    {
-        if (! $this->printButton) {
-            return '';
-        }
-
-        $id = 'ctrl-p-print-button';
-
-        return <<<EOT
-
-            <button onclick='window.print()' id='{$id}'>Print</button>
-            <style>
-            #{$id} {
-                display: block;
-            }
-            </style>
-        EOT;
-    }
-
-    protected function backUrlComponent(): string
-    {
-        if (is_null($this->backUrl)) {
-            return '';
-        }
-
-        $id = 'ctrl-p-back-url';
-
-        return <<<EOT
-
-            <a href='{$this->backUrl}' id='{$id}'>Back</a>
-            <style>
-                #{$id} {
-                    display: block;
-                }
-            </style>
-        EOT;
-    }
-
     protected function compileControlComponents(): string
     {
         if (is_null($this->backUrl) && ! $this->printButton) {
@@ -214,31 +176,63 @@ class CtrlP
         }
 
         $id = 'ctrl-p-control';
-        $backUrlComponent = $this->backUrlComponent();
-        $printButtonComponent = $this->printButtonComponent();
+
+        $backUrlAnchor = ! is_null($this->backUrl) ?
+            "<a href='{$this->backUrl}'>
+                <svg xmlns='http://www.w3.org/2000/svg' class='icon icon-tabler icon-tabler-arrow-back-up' width='16' height='16' viewBox='0 0 24 24' stroke-width='3' stroke='#ffffff' fill='none' stroke-linecap='round' stroke-linejoin='round'>
+                  <path stroke='none' d='M0 0h24v24H0z' fill='none'/>
+                  <path d='M9 14l-4 -4l4 -4' />
+                  <path d='M5 10h11a4 4 0 1 1 0 8h-1' />
+                </svg>
+            </a>" :
+            '';
+
+        $printButton = $this->printButton ?
+            '<button onclick="window.print()">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-printer" width="16" height="16" viewBox="0 0 24 24" stroke-width="3" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <path d="M17 17h2a2 2 0 0 0 2 -2v-4a2 2 0 0 0 -2 -2h-14a2 2 0 0 0 -2 2v4a2 2 0 0 0 2 2h2" />
+                    <path d="M17 9v-4a2 2 0 0 0 -2 -2h-6a2 2 0 0 0 -2 2v4" />
+                    <path d="M7 13m0 2a2 2 0 0 1 2 -2h6a2 2 0 0 1 2 2v4a2 2 0 0 1 -2 2h-6a2 2 0 0 1 -2 -2z" />
+                </svg>
+            </button>' :
+            '';
 
         return <<<EOT
 
         <div id='{$id}'>
-            {$printButtonComponent}
-            {$backUrlComponent}
+            {$printButton}
+            {$backUrlAnchor}
+            <style>
+                div#{$id} {
+                    position: fixed;
+                    bottom: 0;
+                    right: 0;
+                    z-index: 999999;
+                    display: flex;
+                    gap: 4px;
+                    padding: 4px;
+                }
+                div#{$id} > a, div#{$id} > button {
+                    display: block;
+                    padding: 3px 6px;
+                    border-radius: 4px;
+                    background: #333;
+                    color: #fff;
+                    font-weight: bold;
+                }
+                div#{$id} > a:hover, div#{$id} > button:hover {
+                    background: #444;
+                }
+                @media print { #{$id} { display: none !important; } }
+            </style>
         </div>
-        <style>
-            #{$id} {
-                display: flex;
-                position: fixed;
-                bottom: 0;
-                right: 0;
-                z-index: 999999;
-            }
-            @media print { #{$id} { display: none; } }
-        </style>
 
         EOT;
     }
 
     // ========================================================================
-    // ControlComponents
+    // JS
     // ========================================================================
 
     protected function compileJs(): string
